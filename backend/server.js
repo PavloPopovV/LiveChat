b1d0469067a92a4b98f2d04d6d7fe930de109303
PavloPopovV <pavlopopov570@gmail.com>
2024-09-11 12:35:17 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express'
 import dotenv from "dotenv"
 import cors from 'cors'
@@ -5,10 +6,14 @@ import cookieParser from 'cookie-parser';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import router from './routes/index.js'
 import { app, server } from './socket/socket.js';
+import exp from "constants";
 
 // const app = express();
 const PORT = process.env.PORT || 5000
 
+//deploy
+const __dirname = path.resolve()
+//---------
 dotenv.config()
 app.use(cors({
     origin:['http://localhost:3000'],
@@ -16,9 +21,14 @@ app.use(cors({
 }));
 app.use(express.json())
 app.use(cookieParser())
-
 app.use("/api", router);
 
+//deploy
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
+//---------
 server.listen(PORT, () => {
         connectToMongoDB()
     console.log(`Server work on port ${PORT}`)
