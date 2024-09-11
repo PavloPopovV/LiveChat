45138cb8c49f743c3a789c6fd8ba77f4fbc25381
PavloPopovV <pavlopopov570@gmail.com>
2024-09-11 14:02:37 +0300

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
-import path from "path"
-import express from 'express'
-import dotenv from "dotenv"
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-import connectToMongoDB from './db/connectToMongoDB.js';
-import router from './routes/index.js'
-import { app, server } from './socket/socket.js';
-import exp from "constants";
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import router from "./routes/index.js";
+import { app, server } from "./socket/socket.js";
 
-// const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-//deploy
-const __dirname = path.resolve()
-//---------
-dotenv.config()
-app.options('*', cors({
-    origin: ['https://chat-app-prod-d0xl.onrender.com', 'http://localhost:3000'],
-    credentials: true
-}));
-app.use(cors({
-    origin:['https://chat-app-prod-d0xl.onrender.com','http://localhost:3000'],
-    credentials:true,
-}));
-app.use(express.json())
-app.use(cookieParser())
+
+const __dirname = path.resolve();
+
+dotenv.config();
+app.use(
+  cors({
+    origin: [
+      "https://chat-app-prod-d0xl.onrender.com",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api", router);
 
-//deploy
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
-//---------
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
-        connectToMongoDB()
-    console.log(`Server work on port ${PORT}`)
+  connectToMongoDB();
+  console.log(`Server work on port ${PORT}`);
 });
